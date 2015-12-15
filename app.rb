00bce39ebc6aa1188dad3e035e78a0f68b89bf78
tasilio <tgb@inbox.com>
2015-12-15 19:59:09 +0300

    db listing

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require 'sqlite3'
 
 db = SQLite3::Database.new 'testsqlite.sqlite'
 
-db.execute "INSERT INTO Cars (Name, Price) VALUES ('Jaguar',777)"
+db.execute "select * from cars" do |cars|
+	puts cars
+	puts"==="
+end
+	
 
 db.close
